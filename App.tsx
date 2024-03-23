a87e5d04d6c0d5cac562604b00e559d0c6fa4719
Kerem Yilmaz <kryilmaz272@gmail.com>
2024-03-23 14:59:17 +0300

    Deneme

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+import{View,Text,StyleSheet,StatusBar} from "react-native"
+import Header from "./src/components/Header"
+import {colors} from "./src/global/styles"
+
+export default function App(){
+  return(
+    <View style ={styles.container}>
+      <StatusBar
+        barStyle= "light-content"
+        backgroundColor = {colors.statusbar}
+        />
+      <Header title="Hesabım"/>
+
+      
+    </View>
+  )
+}
+
+const styles =StyleSheet.create({
+  container: {flex:1}
+})
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { View,Text,StyleSheet,Dimensions,Image} from "react-native";
+import { colors,parameters } from "../global/styles";
+import {Icon} from "react-native-elements";
+
+
+
+
+
+
+export default function Header({title}){
+  return(
+
+    <View style={styles.header}>
+      <View style={{marginLeft:20}}>
+        
+      <View style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start' }}>
+      <Image
+        style={{ width: 30, height: 30 }}
+        source={require('../screens/solaok.png')}
+      />
+    </View>
+      </View>
+      <View>
+          <Text style={styles.headerText}>{title}</Text>
+        </View>
+    </View>
+
+  ) 
+  
+}
+const styles =StyleSheet.create({
+  header:{
+    flexDirection:"row",
+    backgroundColor:colors.buttons,
+    height:parameters.headerHeight
+  },
+  headerText:{
+    color:colors.headerText,
+    fontSize:22,
+    fontWeight:"bold",
+    marginLeft:30
+  }
+})
