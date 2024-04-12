402c95f13603a1dbc55edc03bf617fbad0faabfb
Kerem Yilmaz <kryilmaz272@gmail.com>
2024-04-13 01:09:41 +0300

    Giriş Ekranındaki Yönlendirme Yapıldı

---
+++ Food-Delivery/Yemek/Yemek/src/navigation/authNavigators.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import {createStackNavigator,TransitionPresets} from "@react-navigation/stack"
+import SingInWelcomeScreen from "../screens/authScreens/SingInWelcomeScreen";
+import { SingInScreen } from "../screens/authScreens/SignInScreen";
+
+const Auth =createStackNavigator();
+
+export default function AuthStack(){
+    return(
+        <Auth.Navigator>
+            <Auth.Screen
+                name="SingInWelcomeScreen"
+                component ={SingInWelcomeScreen}
+                options ={{
+                    headerShown: false,
+                    ...TransitionPresets.RevealFromBottomAndroid
+                }
+                    
+                }
+            />
+
+            <Auth.Screen
+                name="SingInScreen"
+                component ={SingInScreen}
+                options ={{
+                    headerShown: false,
+                    ...TransitionPresets.RevealFromBottomAndroid
+                }
+                    
+                }
+            />
+
+     </Auth.Navigator>
+    )
+}
